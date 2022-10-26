f8b1be1f867ceea45b88dd43d1877f13b26d624e
ggwork <1159468968@qq.com>
2022-10-26 20:40:43 +0800

    修改翻译

--- translateFile.js
+++ translateFile.js
@@ -23,7 +23,7 @@ async function translateWord(content) {
 function getFileInfo(filePath) {
   let fileName = path.basename(filePath)
   let dirName = path.dirname(filePath)
-  let fileOutDir = path.join(outputPath, path.basename(dirName))
+  let fileOutDir = path.join(outputPath, dirName)
   // 文件路径、文件名、文件夹路径、输出文件夹路径
   return {
     filePath,
@@ -38,7 +38,7 @@ function getFileInfo(filePath) {
 async function translateOneFile(filePath) {
   // console.log('开始翻译')
   let content = fs.readFileSync(filePath)
-  content = content.toString().replace('')
+  content = content.toString()
   const translatedContent = await translateWord(content)
   // console.log('translatedContent:',translatedContent)
   
@@ -46,9 +46,9 @@ async function translateOneFile(filePath) {
   let fileDirName_zn = await translateWord(fileInfo.dirName)
   // 文件夹名称  英文-中文
   let fileDir = fileInfo.dirName+'-'+fileDirName_zn
-  let fileOutDir = path.join(outputPath, path.basename(fileDir))
+  let fileOutDir = path.join(outputPath, fileDir)
   // 文件统一改成txt文件
-  await utils.createDataFilePromise(fileInfo.fileName+'.txt', translatedContent, fileOutDir)
+  await utils.createDataFilePromise(fileInfo.fileName+'.html', translatedContent, fileOutDir)
 }
 
 function translateFinally(){
@@ -58,12 +58,32 @@ function translateFinally(){
 async function translateAllFile(dirName){
   // 获取所有文件路径
   const fileList = await utils.getAllFilesByFloderPath(dirName)
+  fileList = fileList.slice(0,2)
 
   utils.getNextItem(0,fileList,async function(filePath,index){
     console.log('正在翻译第'+index+'文件')
     await translateOneFile(filePath)
   },translateFinally)
 }
+
+async function delteOneFileChar(filePath,beDelChar) {
+  // console.log('开始翻译')
+  let content = fs.readFileSync(filePath)
+  content = content.toString().replaceAll(beDelChar,'')
+  let fileInfo = getFileInfo(filePath)
+  
+  await utils.createDataFilePromise(fileInfo.fileName, content, fileInfo.fileOutDir)
+}
+
+// 批量删除文件里的字符
+async function deleteAllFilesChar(dirName,beDelChar){
+  const fileList = await utils.getAllFilesByFloderPath(dirName)
+  utils.getNextItem(0,fileList,async function(filePath,index){
+    console.log('正在处理第'+index+'文件')
+    await delteOneFileChar(filePath,beDelChar)
+  },translateFinally)
+}
+// deleteAllFilesChar('./translateResult','undefined')
 
 // translateOneFile('./data/Albania/authority')
 // 翻译data文件夹下的所有文件
